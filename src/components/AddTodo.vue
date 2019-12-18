a2eb1fa29f55578efe705d29a49b67f89d83aae1
tasleem khan <tasleem.khan@surmountsoft.in>
2019-12-18 10:46:58 +0530

    this is my first todo

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <form @submit.prevent="addTodo">
+      <input type="text" v-model="title" name="title" placeholder="Add Todo...">
+      <br/>
+      <input type="date" v-model="date" name="date">
+      <br/>
+      <select v-model="selected">
+      <option :value="Done" disabled>Done?</option>
+        <option>Yes</option>
+        <option>No</option>
+      </select>
+      <br/>
+      <input type="text" v-model="title" name="title" placeholder="Notes...">
+      <br>
+      <input type="submit" value="Submit" class="btn">
+    </form>
+  </div>
+</template>
+
+<script>
+// import uuid from 'uuid';
+export default {
+  name: "AddTodo",
+  data() {
+    return {
+      title: ''
+    }
+  },
+  methods: {
+    addTodo() {
+      const newTodo = {
+        title: this.title,
+        completed: false
+      }
+      // Send up to parent
+      this.$emit('add-todo', newTodo);
+      this.title = '';
+    }
+  }
+}
+</script>
+
+<style scoped>
+  form {
+    display: flex;
+  }
+  input[type="text"] {
+    flex: 10;
+    padding: 5px;
+  }
+  input[type="submit"] {
+    flex: 2;
+  }
+</style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <Header />
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Header from './components/layout/Header';
+export default {
+  name:"app",
+  components: {
+    Header
+  }
+}
+</script>
+
+
+<style>
+
+</style>
